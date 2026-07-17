55eb0d695f2cab0537a04a7e55e30dbb6c9b9739
FrankyRey <36908431+FrankyRey@users.noreply.github.com>
2026-07-17 12:23:25 -0600

    Update ACATLAN_11_style.js

--- styles/ACATLAN_11_style.js
+++ styles/ACATLAN_11_style.js
@@ -23,7 +23,7 @@ var style_ACATLAN_11 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(133,182,111,0.8)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(133,182,111,0.6)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
